23a6e2fb3a13c3d7511d3ce291a746c4bc0c466d
william20047 <a.william.20047@gmail.com>
2024-11-11 12:11:54 -0500

    Removed old and reconfigured

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,6 @@ module.exports = function(eleventyConfig) {
   
 	eleventyConfig.addPassthroughCopy({"src/other/robot.txt": "/robot.txt"})
 
-  
-	// Check if we're in development mode
-	if(process.env.NODE_ENV === 'production') {
-		// Add a directory of development-only pages
-    	eleventyConfig.ignores.add('src/dev/')
-	}
-	if(process.env.NODE_ENV === 'development'){
-		eleventyConfig.addPassthroughCopy({"src/dev/assets": "dev/assets/"})
-	}
-
 
 	return {
 		markdownTemplateEngine: 'njk',
