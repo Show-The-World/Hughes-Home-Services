85e20aceecc68c04466f55327d6d054d77ba261d
william20047 <a.william.20047@gmail.com>
2024-12-21 21:57:36 -0500

    configured date

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,18 @@
+const {DateTime} = require('luxon');
+
 module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy({"src/styles/": "styles/"})
   	eleventyConfig.addPassthroughCopy({"src/assets/": "assets/"})
   
 	eleventyConfig.addPassthroughCopy({"src/other/robot.txt": "/robot.txt"})
-	
+ 
 	eleventyConfig.addPassthroughCopy({"src/pages/admin/config.yml": "admin/config.yml"})
 
+	eleventyConfig.addFilter("postDate", (dateObj) =>{
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+	})
+	
   
 	return {
 		markdownTemplateEngine: 'njk',
